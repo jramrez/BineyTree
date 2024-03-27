51f2e8d8acb3c3051e1acdb19e16012e525c811d
jramrez <jramrez@ucvts.org>
2024-03-27 09:21:21 -0400

    first

---
+++ Main.java
@@ -0,0 +1,11 @@
+/*Program Name: BineyTree
+ *Program Purpose: Create a print a family tree using a Binary Tree
+ Created / Edited by: jramrez */
+
+
+
+class Main {
+    public static void main(String[] args) {
+        BineyTree famTree = new BineyTree("John");
+    }
+}
